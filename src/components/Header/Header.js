6d64d9238c6ffd9597ffa720eee412ae4e7900b9
HobbyCreate <63106036+HobbyCreate@users.noreply.github.com>
2023-01-03 15:28:55 +0700

    Search bug fixed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,39 +1,44 @@
 import React, { useState, useEffect } from 'react'
 import './Header.css'
 import { AiOutlineShoppingCart } from 'react-icons/ai'
-import { BiSearch } from 'react-icons/bi'
+// import { BiSearch } from 'react-icons/bi'
 import { useDispatch, useSelector } from 'react-redux'
 import { searchProducts, clearSearchProducts } from '../../features/productSlice'
 import { Link } from 'react-router-dom'
 
 function Header() {
     const dispatch = useDispatch();
-    const [temp, setTemp] = useState([])
+    const [result, setResult] = useState([])
+    const [search, setSearch] = useState('')
     const items = useSelector(state => state.products.items)
     const cartCount = useSelector(state => state.cart.cartLength);
-    let tempArr = [];
 
-    const onBlurHandler = (e) => {
-        const search = e.target.value;
-        if (search !== '' || search !== 'undefined') {
-            if (items !== 'undefined') {
-                (items.map(item =>
-                    item.title.toLowerCase().includes(search) ? tempArr.push(item) : tempArr
-                ));
-            }
-            setTemp(tempArr)
-        }
+    const onChangeHandler = (e) => {
+        setSearch((e.target.value).toLowerCase())
     }
+
+    const searchByProducts = (e) => {
+        if (e.key === "Enter") {
+            if (search !== '' || search !== 'undefined') {
+                if (items !== 'undefined') {
+                    const tempArr = items.filter((item) =>
+                        item.title.toLowerCase().includes(search))
+                    setResult(tempArr)
+                }
+            }
+        }
+
+    };
 
     useEffect(() => {
         dispatch(clearSearchProducts())
-        dispatch(searchProducts(temp))
-    }, [dispatch, temp])
+        dispatch(searchProducts(result))
+    }, [dispatch, result])
 
     return (
         <div className='header-container'>
             <div className='shop-name'>
-                <Link to='/' onClick={dispatch(clearSearchProducts())}><h1>All Around The World Shop</h1></Link>
+                <Link to='/' onClick={() => dispatch(clearSearchProducts())}><h1>All Around The World Shop</h1></Link>
             </div>
             <div className='functional'>
                 <div className='dark-mode'></div>
@@ -44,8 +49,13 @@ function Header() {
                     </div>
                 </Link>
                 <div className='search-container'>
-                    <input type="text" placeholder="Search.."  onBlur={onBlurHandler} />
-                    <BiSearch className='search-icon' />
+                    <input
+                        type="text"
+                        placeholder="Search.."
+                        onChange={onChangeHandler}
+                        value={search}
+                        onKeyUp={searchByProducts}
+                    />
                 </div>
             </div>
         </div>
--- src/components/homePage/HomePage.js
+++ src/components/homePage/HomePage.js
@@ -7,26 +7,25 @@ import CardComponents from '../Card/CardComponents';
 
 function HomePage() {
     const dispatch = useDispatch();
-
     useEffect(() => {
         const fetchProducts = async () => {
             const response = await axios.get('https://fakestoreapi.com/products')
                 .catch((error) => {
                     console.log(error);
                 });
-                dispatch(clearAllProducts());
-                dispatch(allProducts(response.data));
+            dispatch(clearAllProducts());
+            dispatch(allProducts(response.data));
         };
         fetchProducts()
     }, [dispatch]);
-    
+
     return (
         <div className='home-container'>
             <div className='category'>
                 <h2>All Products</h2>
             </div>
             <div className='products'>
-                <CardComponents/>
+                <CardComponents />
             </div>
 
         </div>
